0d93b39511e42cb7854a598950807c5c5087208f
Davi Viana <davipviana@gmail.com>
2018-11-11 18:46:29 -0200

    Refactoring package description activity

--- app/src/main/java/com/davipviana/vacationpackages/ui/activity/PackageDescriptionActivity.kt
+++ app/src/main/java/com/davipviana/vacationpackages/ui/activity/PackageDescriptionActivity.kt
@@ -13,45 +13,69 @@ import java.util.*
 
 class PackageDescriptionActivity : AppCompatActivity() {
 
+    companion object {
+        const val APPBAR_TITLE : String = "Resumo do pacote"
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_package_description)
 
-        title = "Resumo do pacote"
+        title = APPBAR_TITLE
 
         val saoPauloVacationPackage = VacationPackage("São Paulo", "sao_paulo_sp",
             2, BigDecimal("243.99"))
 
-        val txtLocal = findViewById<TextView>(R.id.package_description_local)
-        txtLocal.text = saoPauloVacationPackage.local
+        setLocalInfo(saoPauloVacationPackage)
 
+        setImageInfo(saoPauloVacationPackage)
+
+        setDaysInfo(saoPauloVacationPackage)
+
+        setPriceInfo(saoPauloVacationPackage)
+
+        setDateInfo(saoPauloVacationPackage)
+    }
+
+    private fun setDateInfo(vacationPackage: VacationPackage) {
+        val txtDate = findViewById<TextView>(R.id.package_description_date)
+        val departureDate = Calendar.getInstance()
+        val arrivalDate = Calendar.getInstance()
+        arrivalDate.add(Calendar.DATE, vacationPackage.days)
+        val dateFormat = SimpleDateFormat("dd/MM")
+        txtDate.text = dateFormat.format(departureDate.time) +
+                " - " +
+                dateFormat.format(arrivalDate.time) +
+                " de " +
+                arrivalDate.get(Calendar.YEAR)
+    }
+
+    private fun setPriceInfo(vacationPackage: VacationPackage) {
+        val txtPrice = findViewById<TextView>(R.id.package_description_price)
+        txtPrice.text = vacationPackage.price.toCurrencyString("pt", "br")
+    }
+
+    private fun setDaysInfo(vacationPackage: VacationPackage) {
+        val txtDays = findViewById<TextView>(R.id.package_description_days)
+        txtDays.text = vacationPackage.days.toString() +
+                if (vacationPackage.days > 1)
+                    " dias"
+                else
+                    " dia"
+    }
+
+    private fun setImageInfo(vacationPackage: VacationPackage) {
         val imgLocal = findViewById<ImageView>(R.id.package_description_image)
         val imageId = this.resources.getIdentifier(
-            saoPauloVacationPackage.image,
+            vacationPackage.image,
             "drawable",
             this.packageName
         )
         imgLocal.setImageDrawable(this.resources.getDrawable(imageId))
+    }
 
-        val txtDays = findViewById<TextView>(R.id.package_description_days)
-        txtDays.text = saoPauloVacationPackage.days.toString() +
-                if (saoPauloVacationPackage.days > 1)
-                    " dias"
-                else
-                    " dia"
-
-        val txtPrice = findViewById<TextView>(R.id.package_description_price)
-        txtPrice.text = saoPauloVacationPackage.price.toCurrencyString("pt", "br")
-
-        val txtDate = findViewById<TextView>(R.id.package_description_date)
-        val departureDate = Calendar.getInstance()
-        val arrivalDate = Calendar.getInstance()
-        arrivalDate.add(Calendar.DATE, saoPauloVacationPackage.days)
-        val dateFormat = SimpleDateFormat("dd/MM")
-        txtDate.text = dateFormat.format(departureDate.time) +
-                        " - " +
-                        dateFormat.format(arrivalDate.time) +
-                        " de " +
-                        arrivalDate.get(Calendar.YEAR)
+    private fun setLocalInfo(vacationPackage: VacationPackage) {
+        val txtLocal = findViewById<TextView>(R.id.package_description_local)
+        txtLocal.text = vacationPackage.local
     }
 }
