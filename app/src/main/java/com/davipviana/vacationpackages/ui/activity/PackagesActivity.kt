342627b1d9c75aa70a569ea20fdbc6b82101abcc
Davi Viana <davipviana@gmail.com>
2018-11-11 11:44:21 -0200

    Activity packages. Positioning header and ListView

---
+++ app/src/main/java/com/davipviana/vacationpackages/ui/activity/PackagesActivity.kt
@@ -0,0 +1,13 @@
+package com.davipviana.vacationpackages.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.davipviana.vacationpackages.R
+
+class PackagesActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_packages)
+    }
+}
